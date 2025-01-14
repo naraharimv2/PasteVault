537caca83270f6bdfc37236713ab418e35c38f64
Narahari M V <97274974+naraharimv2@users.noreply.github.com>
2025-01-14 22:17:07 +0530

    Delete backend/pastebin directory

--- backend/pastebin/src/main/java/com/example/pastebin/exeption/PasteNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.pastebin.exeption;
-
-public class PasteNotFoundException extends RuntimeException {
-    
-    public PasteNotFoundException(String message) {
-        super(message);
-    }
-}
